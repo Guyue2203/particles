fe3e0802428a8e21cfa01909228f60356ea38283
沽月 <2583526329@qq.com>
2025-12-04 20:08:21 +0800

    update workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Removed 'base' config which was for GitHub Pages
+  base: '/particles/',
   server: {
     host: true, // Listen on all local IPs
     open: true  // Open browser on start
